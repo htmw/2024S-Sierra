95e26844b0c2f42f4c11e05389a18e8ebc74939c
Satishindla <isatishreddy20@gmail.com>
2024-05-10 15:28:09 +0530

    Improve TensorFlow model handling with validation checks

--- code/backend/ml_api/modelLoader.js
+++ code/backend/ml_api/modelLoader.js
@@ -1,18 +1,53 @@
+// const tf = require("@tensorflow/tfjs-node");
+
+// let globalModel;
+
+// async function loadModel() {
+//   try {
+//     globalModel = await tf.loadLayersModel("file://./model/model.json");
+//   } catch (error) {
+//     throw error;
+//   }
+// }
+
+// function getModel() {
+//   return globalModel;
+// }
+
+// module.exports = {
+//   loadModel,
+//   getModel,
+// };
+
+
+
+// ////////////////////////////////////////////// updated /////////////////////////////////////
+
+
+
+
 const tf = require("@tensorflow/tfjs-node");
 
-let globalModel;
+// Global variable to hold the loaded model
+let globalModel = null;
 
-async function loadModel() {
+// Load the model from the specified path
+const loadModel = async () => {
   try {
     globalModel = await tf.loadLayersModel("file://./model/model.json");
   } catch (error) {
-    throw error;
+    console.error("Error loading model:", error);
+    throw new Error("Model loading failed");
   }
-}
+};
 
-function getModel() {
+// Get the loaded model
+const getModel = () => {
+  if (!globalModel) {
+    throw new Error("Model not loaded yet");
+  }
   return globalModel;
-}
+};
 
 module.exports = {
   loadModel,
